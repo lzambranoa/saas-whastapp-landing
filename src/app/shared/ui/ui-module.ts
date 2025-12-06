4027c08bf8bf8acb71dca3adb102e39c50884ffd
Leonardo Zambrano <lzambranoa.software@gmail.com>
2025-12-06 12:23:16 -0500

    configuracion colores tailwind y exportacion ui.module

--- src/app/shared/ui/ui-module.ts
+++ src/app/shared/ui/ui-module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button/button-component/button-component';
+import { CardComponent } from './card/card.component/card.component';
+import { SectionComponent } from './section/section.component/section.component';
+import { InputComponent } from './input/input.component/input.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ButtonComponent,
+    InputComponent,
+    CardComponent,
+    SectionComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ButtonComponent,
+    InputComponent,
+    CardComponent,
+    SectionComponent,
   ]
 })
 export class UiModule { }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#542C49',
+          dark: '#3E2137',
+          light: '#7A4F71',
+        },
+        secondary: {
+          DEFAULT: '#E38AAE',
+          dark: '#C76F93',
+        },
+      },
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
   },
   plugins: [],
 };
