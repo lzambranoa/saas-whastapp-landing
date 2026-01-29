f18a677de3e6334987f6fe749bb0cfb81488f133
Leonardo Zambrano <lzambranoa.software@gmail.com>
2026-01-29 14:49:04 -0500

    Se realiza ajuste de navbar-builder

--- src/app/landing-builder/components/builder-navbar/builder-navbar/builder-navbar.ts
+++ src/app/landing-builder/components/builder-navbar/builder-navbar/builder-navbar.ts
@@ -1,17 +1,30 @@
-import { Component, computed, inject } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-builder-navbar',
-  imports: [RouterModule],
+  imports: [],
   templateUrl: './builder-navbar.html',
   styleUrl: './builder-navbar.css',
 })
 export class BuilderNavbar {
 
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
-  landingId = computed(() =>
-    this.route.snapshot.paramMap.get('id')
-  );
+   get isPreview(): boolean {
+    return this.router.url.includes('/preview');
+  }
+
+  goEdit() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  goPreview() {
+    this.router.navigate(['preview'], { relativeTo: this.route });
+  }
+
+  goBack() {
+    this.router.navigate(['/dashboard/home']);
+  }
 }
--- src/app/landing-builder/pages/preview/preview.page.ts
+++ src/app/landing-builder/pages/preview/preview.page.ts
@@ -1,11 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { RenderService } from '../../services/render.service';
 
 import { HeroSection } from '../../components/hero-section/hero-section';
 import { FeaturesSection } from '../../components/features-section/features-section';
 import { CtaSection } from '../../components/cta-section/cta-section';
+import { LandingsServices } from '../../services/landings.service';
 import { BuilderNavbar } from "../../components/builder-navbar/builder-navbar/builder-navbar";
 
 @Component({
@@ -22,12 +22,13 @@ import { BuilderNavbar } from "../../components/builder-navbar/builder-navbar/bu
 export class PreviewPage implements OnInit {
 
   private route = inject(ActivatedRoute);
-  render = inject(RenderService);
+  landings = inject(LandingsServices);
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (!id) return;
 
-    this.render.loadLanding(id);
+    // 🔑 misma landing, misma fuente de verdad
+    this.landings.select(id);
   }
 }
